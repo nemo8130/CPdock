1b49c37159a482f42fef82b487437743aa4015de
Sankar Basu <nemo8130@gmail.com>
2021-07-20 22:47:16 +0530

    metal coordination debugged

--- EXEC/metcoord.f
+++ EXEC/metcoord.f
@@ -8,8 +8,10 @@
       character(4)::atag
       character(3)::atype,rtype
       character(3)::patom(20000),pres(20000),matom(100),mres(100)
-      character(3)::prs,mco(30)
-      character(1)::pchain(20000),mchain(20000),chn,mch(30),pch,pch1(30)
+      character(3)::patom1(100)
+      character(3)::prs(30),mco(30)
+      character(1)::pchain(20000),mchain(20000),chn,mch(30)
+      character(1)::pch(30),pch1(30)
       real::xp1(30),yp1(30),zp1(30)
       integer::ipres(20000),imres(100),iprs1(30)
       real::xp(20000),yp(20000),zp(20000),xm(100),ym(100),zm(100)
@@ -29,7 +31,7 @@
       ic2 = 0
 
 9     read(1,34,end=30)atag,atype,rtype,chn,irn,xn,yn,zn
-!      write(*,34)atag,atype,rtype,chn,irn,xn,yn,zn
+!      write(872,34)atag,atype,rtype,chn,irn,xn,yn,zn
           if (atag.eq.'ATOM')then
               if (atype(1:1).ne.'H')then
               ic1 = ic1 + 1
@@ -54,26 +56,40 @@
        goto 9
 30     continue
 
-34     format(a4,9x,a3,1x,a3,1x,a1,1x,i3,4x,3f8.3)
+34     format(a4,9x,a3,1x,a3,1x,a1,i4,4x,3f8.3)
 
-!       print*,ic1,ic2
+       print*,ic1,ic2
+
+!       do i=1,ic1
+!       write(872,819)patom(i),pres(i),ipres(i),pchain(i)
+!       enddo
+
+!       write(872,*)
+!       write(872,*)
+!       write(872,*)
 
 !================================================
 !      CONTACNT CUTOFF 4 Angstrom
 !================================================
+
+819    format(a4,1x,a3,1x,i4,1x,a1,3f8.3)
+919    format(a4,1x,a3,1x,i4,1x,a1,3f8.3,2x,f10.3)
 
        ir1 = ipres(1)
        irl = ipres(ic1)
        Nco = 0
 
        do j = 1,ic2             ! On Metal ions
+!       write(873,819)matom(j),mres(j),imres(j),mchain(j),
+     &xm(j),ym(j),zm(j)
           do k = ir1,irl        ! On residues
           ik = 0
              do i = 1,ic1       ! On protein atoms
                if (k==ipres(i))then
                ik = ik + 1
-               pch = pchain(i)
-               prs = pres(i)
+               patom1(ik) = patom(i)
+               pch(ik) = pchain(i)
+               prs(ik) = pres(i)
                xp1(ik) = xp(i)
                yp1(ik) = yp(i)
                zp1(ik) = zp(i)
@@ -85,6 +101,8 @@
              dist = sqrt((xp1(ii)-xm(j))**2 + (yp1(ii)-ym(j))**2 + 
      &(zp1(ii)-zm(j))**2)
                 if (dist <= 4.0)then
+!                write(874,919)patom1(ii),prs(ii),k,pch(ii),
+!     &xp1(ii),yp1(ii),zp1(ii),dist
                 ircon = ircon + 1
                 imco(ircon) = imres(j)
                 mco(ircon) = mres(j)
@@ -94,13 +112,20 @@
              if (ircon >= 1)then
              Nco = Nco + 1
             write(67,57)imco(ircon),'-',mco(ircon),'-',mch(ircon),
-     &'<=>',k,'-',prs,'-',pch,ircon
-             pch1(Nco) = pch
+     &'<=>',k,'-',prs(ircon),'-',pch(ircon),ircon
+             pch1(Nco) = pch(ircon)
              iprs1(Nco) = k
 !             print*,iprs1(Nco),pch1(Nco)
              endif
           enddo
        enddo
+
+!       print*,Nco
+
+!       do i=1,Nco
+!       print*,pch1(i)
+!       enddo
+
 
        str1 = 'select '
        do i = 1,Nco
@@ -128,6 +153,7 @@
        write(68,96)'color chain'
        write(68,93)'select hetero'
        write(68,97)'spacefill'
+       write(68,*)'color yellow'
 
        if (ils2 < 100)then
        write(68,94)adjustl(str1)
@@ -159,7 +185,7 @@
 
 89     format(1x,a1,a1,1x,a2,1x,i3,a1)
 189    format(1x,a1,a1,1x,a2,1x,i3)
-57     format(i3,a1,a3,a1,a1,1x,a3,1x,i3,a1,a3,a1,a1,2x,i5)
+57     format(i4,a1,a3,a1,a1,1x,a3,1x,i4,a1,a3,a1,a1,2x,i5)
 
 
       endprogram metcoord
